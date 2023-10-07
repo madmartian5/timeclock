edeab7eb4134463843cecdae4868a3b5479766f6
madmartian5 <am.experts.online@gmail.com>
2023-10-07 20:24:29 +1300

    hiding socials and also testing if localstorage with update

--- src/lib/globals.ts
+++ src/lib/globals.ts
@@ -4,9 +4,9 @@ export const globalProperties = {
     version: 'v0.0.1',
     testmode: false,
     socials: [
-        { name: 'twitter', link: 'https://twitter.com/EmpoweredDAO' },
-        { name: 'discord', link: 'https://discord.gg/PcgPRWmSz3' },
-        { name: 'github', link: 'https://github.com/Empowered-DAO' },
+        //{ name: 'twitter', link: 'https://twitter.com/EmpoweredDAO' },
+        //{ name: 'discord', link: 'https://discord.gg/PcgPRWmSz3' },
+        //{ name: 'github', link: 'https://github.com/Empowered-DAO' },
         // Add more social media entries here - make sure there is a component for it in src/assets
     ],
   };
