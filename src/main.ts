3cbbe3079b2112261e701eb4d04ac253f774f3da
madmartian5 <am.experts.online@gmail.com>
2023-10-08 23:07:02 +1300

    working on pwa

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,20 @@ app.use(pinia)
 
 async function setupApp() {
   try {
-
+    // Register the service worker
+    if ('serviceWorker' in navigator) {
+      await navigator.serviceWorker.register('/sw.js')
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
+    }
   } catch (error) {
     console.error("Error during app setup:", error);
   }
 }
-
 
 setupApp().then(() => {
   app.use(router);
